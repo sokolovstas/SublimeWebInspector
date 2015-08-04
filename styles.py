4433900918194da3262d08fb95fb0ec84fb161f5
danmosemsft <danmose@microsoft.com>
2015-08-03 22:40:00 -0700

    Foundation of styles communication.
    Next up: get channel to work in styles.py.

--- styles.py
+++ styles.py
@@ -1,19 +1,31 @@
-
 import sublime
 import sublime_plugin
-
 import views
+import utils
+import webkit
+from webkit import DOM
+from webkit import CSS
 
-class SwiStylesWindowCommand(sublime_plugin.WindowCommand):
-    """ Initializes the Styles pane """
+#from protocol import channel
+import protocol
+# do not import swi
 
-    def run(self):
-        v = views.find_or_create_view('styles')
-        v.run_command('swi_styles_window_internal')
+def show_styles():
+    v = views.find_or_create_view('styles')
+    v.run_command('swi_styles_window_internal')
 
 
 class SwiStylesWindowInternalCommand(sublime_plugin.TextCommand):
     """ Called internally on the correct view """
 
     def run(self, edit):
-        self.view.insert(edit, 0, 'hello world')
+        v = views.wrap_view(self.view)
+        v.print_click(edit, v.size(), "  Inspect Element  ", self.view.window().run_command, 'swi_styles_inspect_element')
+
+
+class SwiStylesInspectElement(sublime_plugin.WindowCommand):
+    def run(self):
+        utils.assert_main_thread()
+        #channel.send(webkit.DOM.setInspectModeEnabled())
+
+    #def elementSelected(self, data):
--- webkit/DOM.py
+++ webkit/DOM.py
@@ -1,1 +1,73 @@
-# not implemented now
+﻿from .wkutils import Command, Notification, WebkitObject
+from .Runtime import RemoteObject
+import json
+import re
+
+def getDocument():
+    command = Command('DOM.getDocument', params)
+    return command
+
+def getDocument_parser():
+    data = {}
+    data['root'] = Node(result['root'])
+
+class Node(WebkitObject):
+    def __init__(self, value):
+        self.set(value, 'nodeId')
+        self.set(value, 'nodeName')
+        #self.set(value, 'nodeValue')
+        self.children= []
+        if 'children' in value:
+            for child in value['children']:
+                self.children.append(Node(child))
+
+def setInspectModeEnabled():
+    params = {}
+    params['enabled'] = "true"
+
+    # these are the values that Chrome inspector uses itself
+    highlightConfig = {}
+    highlightConfig['showInfo'] = true
+    highlightConfig['showRulers'] = true
+    highlightConfig['showExtensionLines'] = false
+    highlightConfig['contentColor'] = json.loads("""
+        "contentColor": { "r": 111, "g": 168, "b": 220, "a": 0.66 },
+        "paddingColor": { "r": 147, "g": 196, "b": 125, "a": 0.55 },
+        "borderColor": { "r": 255, "g": 229, "b": 153, "a": 0.66 },
+        "marginColor": { "r": 246, "g": 178, "b": 107, "a": 0.66 },
+        "eventTargetColor": { "r": 255, "g": 196, "b": 196, "a": 0.66 },
+        "shapeColor": { "r": 96, "g": 82, "b": 177, "a": 0.8 },
+        "shapeMarginColor": { "r": 96, "g": 82, "b": 127, "a": 0.6 }
+    """)
+   
+    params['highlightConfig'] = highlightConfig
+    command = Command('DOM.setInspectModeEnabled', {})
+    return command
+
+def inspectNodeRequested():
+    return Notification('DOM.inspectNodeRequested')
+
+def inspectNodeRequested_parser(params):
+    return params['backendNodeId']
+
+def pushNodesByBackendIdsToFrontEnd(backendNodeIds):
+    return Command('DOM.pushNodesByBackendIdsToFrontEnd', {"backendNodeIds": backendNodeIds })
+
+def pushNodesByBackendIdsToFrontEnd_parser(params):
+    return params['nodeIds'] #array
+
+def setChildNodes():
+    return Notification('DOM.setChildNodes')
+
+def setChildNodes_parser(params):
+    data = {}
+    data['parentId'] = params['parentId']
+    data['nodes'] = []
+    for node in params['nodes']:
+        data['nodes'].append(Node(node))
+
+def requestChildNodes(nodeId):
+    return Command('DOM.requestChildNodes', { "nodeId": nodeId })
+
+
+
