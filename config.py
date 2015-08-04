6a353b084d9dfc7a178dc4b66843920ceb30c0ae
dan moseley <danmose@microsoft.com>
2015-08-03 22:00:15 -0700

    Remove unnecessary import cycle

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 
 # this contains all globals that are used by 
 # modules that are imported in more than one place.
-# if modules X and Y both import Z, then
-# two instances of any global in Z will
-# exist, causing subtle problems.
+#
+# if there is a circle of imports then globals
+# can be multiply instantiated, once initially,
+# then again when the file is cyclically imported
 #
 # but if the global is factored into config.py
-# and config.py is only imported by Z,
+# and config.py imports nothing itself,
 # only one instance of the global will exist.
 buffers  = {}
