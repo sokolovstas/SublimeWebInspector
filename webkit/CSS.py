1aa7125dedda0e119f1de889146d64ed3e126b18
dan moseley <danmose@microsoft.com>
2015-08-03 22:40:00 -0700

    Foundation of styles communication.
    Next up: get channel to work in styles.py.

---
+++ webkit/CSS.py
@@ -0,0 +1,20 @@
+﻿from .wkutils import Command, Notification, WebkitObject
+from .Runtime import RemoteObject
+import json
+import re
+
+def getMatchedStylesForNode(nodeid):
+    return Command('CSS.getMatchedStylesForNode', { "nodeId": nodeId })
+
+#def getMatchedStylesForNode_parser(params):
+#    data = {}
+#    for match in params['matchedCSSRules']:
+#        data['matchedCSSRules'].append(RuleMatch(match))
+
+#class RuleMatch:
+#    def __init__(self, value):
+#        self.rule = Rule(value['rule'])
+#        self.matchingSelectors= []
+#        if 'children' in value:
+#            for child in value['children']:
+#                self.children.append(Node(child))
